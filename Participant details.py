fa5c40e18b9a4f54aa12cfa8f7abab929fe717a1
M.Arun prasath <70267488+techyarun@users.noreply.github.com>
2021-08-18 14:42:06 +0530

    Create Participant details.py

---
+++ Participant details.py
@@ -0,0 +1,51 @@
+#!C:/Users/---->Your Folder Name<----/AppData/Local/Programs/Python/Python39/python.exe
+
+print("Content-type:text/html \r\n\r\n")
+import cgi,pymysql,cgitb;cgitb.enable()
+conn=pymysql.connect(host="localhost",user="root",password="",database="sports")
+cur=conn.cursor()
+f=cgi.FieldStorage()
+id=f.getvalue("id")
+q="""select * from participant"""
+cur.execute(q)
+r=cur.fetchall()
+count=0
+print("""
+    <html>
+    <head><title>my profile</title></head>
+    <style>
+        table,th,td{
+        border-collapse:collapse;
+        border:3px solid powderblue;
+        padding:30px;
+        }
+    </style>
+    <body>
+    <table border="2" width="50">
+    <tr>
+    <th>id</th>
+    <th>ptid</th><th>name</th><th>gender</th><th>email</th><th>contact</th><th>age</th><th>game</th><th>bloodgroup</th><th>username</th>
+    <th>password</th><th>photo</th>
+    </tr>""")
+for i in r:
+    count=count+1
+    print("""
+    <td>%s</td>
+    <td>%s</td>
+    <td>%s</td>
+    <td>%s</td>
+    <td>%s</td>
+    <td>%s</td>
+    <td>%s</td>
+    <td>%s</td>
+    <td>%s</td>
+    <td>%s</td>
+    <td>%s</td>
+    <td><img src="image/%s" width="30" height="40"></td></tr>
+"""%(count,i[1],i[2],i[3],i[4],i[5],i[6],i[7],i[8],i[9],i[10],i[11]))
+print("""
+</table>
+</body>
+</html>
+""")
+conn.close()
